60cb9143eeebead90f65d157858de5b8451a88a1
rf-007 <knightsea05@gmail.com>
2022-12-23 20:15:03 +0600

    Added Layout Files

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -1,15 +1,19 @@
-import classes from "./Hero.module.css"
+import Image from "next/image";
+import Logo from "../public/images/site/image.jpg"
+import classes from "./Hero.module.css";
 
 function Hero() {
-    return(
-        <>
-          <section className={classes.hero}>
-                <div className={classes.image}></div>
-                <h1>Hello I am John</h1>
-                <p>I develop websites in my free time and play video games</p>
-          </section>
-        </>
-    )
+  return (
+    <>
+      <section className={classes.hero}>
+        <div className={classes.image}>
+            <Image src={Logo} alt = "logo" width={300} height={300}/>
+        </div>
+        <h1>Hello I am John</h1>
+        <p>I develop websites in my free time and play video games</p>
+      </section>
+    </>
+  );
 }
 
-export default Hero
+export default Hero;
--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,16 @@
 import '../styles/globals.css'
+import Layout from '../components/layout/layout'
+
+import MainNavigation from '../components/layout/main-navigation'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+    <Layout>
+    <Component {...pageProps} />
+    </Layout>
+    </>
+  )
+  
+  
 }
